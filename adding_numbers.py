eb314417d99706a28a81690a989b9be38b735986
Amneet <kauramneet986@gmail.com>
2024-01-23 15:17:20 +0000

    added third number in a python file

--- adding_numbers.py
+++ adding_numbers.py
@@ -1,13 +1,14 @@
-def add_two_numbers(num1, num2):
-    return num1 + num2
+def add_two_numbers(num1, num2,num3):
+    return num1 + num2 + num3
 
 # Get input from the user
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
+num3 = float(input("Enter the third number: "))
 
 # Call the function to add the numbers
-result = add_two_numbers(num1, num2)
+result = add_two_numbers(num1, num2,num3)
 
 # Display the result
-print(f"The sum of {num1} and {num2} is: {result}")
+print(f"The sum of {num1} , {num2}and {num3} is: {result}")
 
